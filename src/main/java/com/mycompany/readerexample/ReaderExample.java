60107ab1a9728ca7fdc66203467a8692d305b080
Günter Pretterhofer <guenter.pretterhofer@gmail.com>
2016-02-21 09:56:31 +0100

    initial commit

---
+++ src/main/java/com/mycompany/readerexample/ReaderExample.java
@@ -0,0 +1,88 @@
+package com.mycompany.readerexample;
+
+import javaslang.control.Failure;
+import javaslang.control.Success;
+import javaslang.control.Try;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+interface BankConnection {
+
+}
+
+class Balance {
+
+    public final BigDecimal amount;
+
+    public Balance(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
+
+class InsufficientBalanceError extends Exception {
+}
+
+class Account {
+
+    public final String owner;
+    public final String number;
+    public final Balance balance;
+
+    public Account(String owner, String number, Balance balance) {
+        this.owner = owner;
+        this.number = number;
+        this.balance = balance;
+    }
+
+    public Account credit(BigDecimal value) {
+        return new Account(owner, number, new Balance(value));
+    }
+
+    public Try<Account> debit(BigDecimal value) {
+        if (balance.amount.compareTo(value) < 0) {
+            return new Failure<>(new InsufficientBalanceError());
+        }
+        return new Success<>(new Account(
+                owner, number, new Balance(balance.amount.subtract(value))));
+    }
+}
+
+interface BankService {
+
+    static TryReader<BankConnection, Account> open(String owner, String number, BigDecimal balance) {
+        if (balance.compareTo(BigDecimal.ZERO) < 0) {
+            return new TryReader<>(bc -> new Failure<>(new InsufficientBalanceError()));
+        }
+        return new TryReader<>(bc -> new Success<>(new Account(owner, number, new Balance(balance))));
+    }
+
+    static Reader<BankConnection, Account> credit(Account account, BigDecimal value) {
+        return new Reader<>(bc -> new Account(account.owner, account.number,
+                new Balance(account.balance.amount.add(value))));
+    }
+
+    static TryReader<BankConnection, Account> debit(Account account, BigDecimal value) {
+        if (account.balance.amount.compareTo(value) < 0) {
+            return new TryReader<>(bc -> new Failure<>(new InsufficientBalanceError()));
+        }
+        return new TryReader<>(bc -> new Success<>(new Account(account.owner, account.number,
+                new Balance(account.balance.amount.subtract(value)))));
+    }
+
+    static void main(String[] args) {
+        TryReader<BankConnection, Account> reader = open("Alice", "123", new BigDecimal(100.0))
+                .mapReader(a -> credit(a, new BigDecimal(200.0)))
+                .mapReader(a -> credit(a, new BigDecimal(300.0)))
+                .flatMap(a -> debit(a, new BigDecimal(400)));
+
+        Try<Account> account = reader.apply(new BankConnection() {
+        });
+
+        assert Objects.equals(account.get().owner, "Alice1");
+        assert Objects.equals(account.get().number, "123");
+        assert account.get().balance.amount.equals(new BigDecimal(200));
+
+        System.out.println("amount = " + account.get().balance.amount);
+    }
+}
